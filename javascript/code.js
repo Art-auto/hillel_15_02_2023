0e929a923f8b154cf86c6b7676adaf1441556dd6
Artem Shoiko <artem@tabeo.co.uk>
2023-04-29 10:19:02 +0300

    CSS lesson

---
+++ javascript/code.js
@@ -0,0 +1,250 @@
+// console.log('Hello Artem')
+
+// коментар в одному рядку
+// TODO: додай перевірку значення
+/*
+коментра в декілька рядків
+рядок 2
+рядок 3
+*/
+
+
+
+
+
+
+
+// var apples       // оголошуємо змінну
+
+// apples = 'Hello test'       // назначаємо значення для змінної
+// var chess = 6
+// console.log(apples)
+
+
+
+
+// console.log(a)
+
+// var a = 6
+
+
+// a = 'text'
+// a = true
+
+
+
+
+
+
+
+// Імена змінних: букви, цифри, символи $ i _
+// Першим символом не може бути цифра
+
+// var testName = 'Hillel';
+// var test_name = 'Hillel';
+// var test1 = 'Hillel';
+// var $ = 'Hillel';
+// var _ = 'Hillel';
+
+
+
+// // Регістр має значення
+
+// var lOgin
+// var login
+// Login = 'name'
+
+
+
+
+// // Так не можна називати змінні:
+
+// var 1login='test'
+// var log-in='test'
+// var var = 'test'
+
+
+
+
+// Типи данних
+// var arr = new Date()
+// console.log()
+/*
+number
+string
+boolean
+object
+null
+undefined
+bigint
+symbol
+
+Обʼєкт може містити в собі:
+
+1. object
+2. array
+3. date 
+*/
+
+// Numbers:
+
+// var length 
+// console.log(typeof length)
+// length = 16
+// console.log(typeof(length))
+
+// Strings:
+// var color = "Yellow";
+// var lastName = "Johnson";
+// var amount = "14453";
+// console.log(typeof(amount))
+
+// // Booleans
+// var x = true;
+// var y = false;
+// console.log(typeof(y))
+// console.log(typeof true)
+// console.log(typeof(null))
+
+
+// var b = 1
+// console.log(typeof b)
+// b = "test"
+// console.log(typeof b)
+// b = true
+// console.log(typeof b)
+
+
+
+
+// // Object:
+// var person = {firstName:"John", lastName:"Doe"};
+
+// // Array object:
+// var cars = ["Saab", "Volvo", "BMW"];
+
+// // Date object:
+// var date = new Date("2022-03-25"); 
+
+
+
+
+// // Константи
+// const PI = 3.14
+
+// let test = 'test'
+// test = 345
+// console.log(test)
+
+
+
+
+// Унарні оператори
+
+/**
+ -
+ +
+ !
+ --
+ ++
+ */
+
+//  // Інкримент префіксний
+//  var a = 5
+//  console.log(++a)
+//  console.log(++a)
+
+//  // Декримент префіксний
+//  console.log(--a)
+//  console.log(--a)
+
+ // Інкримент постфіксний
+ 
+// var b = 5
+// console.log(b++)
+// console.log(b++)
+// console.log(b)
+//  // Декримент постфіксний
+// console.log(b--)
+// console.log(b--)
+
+// var isHillelStudent = true
+// console.log(!isHillelStudent)
+
+
+// ------------------------------------------------
+
+// + конкатенація
+
+// var a = '5'
+// var b = 2
+// var c = a + b
+// console.log(c)
+// console.log(typeof c)
+
+// var x = 3
+// var a = '6'
+// var c = x + a
+// console.log(typeof a)
+// console.log( +a)
+// console.log(typeof +a)
+// console.log(parseInt(a))
+// console.log(x-a)
+
+
+
+// оператори порівняння
+// 
+// >, <, >=, <=, !=, ==
+//
+// повертають true або false
+
+// console.log(5>7)
+// console.log('2'>1)
+// console.log('001'==1)
+// console.log(true == 1)
+// console.log(false < 1)
+// console.log('001'===1)
+
+
+// порівняння рядків
+// console.log('a'>'A')  // маленькі букви більше великих букв
+// console.log('A'>'1')  // великі букви більше цифр
+// console.log('1'>'$')  // цифри більше символів
+
+// console.log('Bee' > 'Azzzzzzzzzz')
+// console.log('love'> 'lose')
+// console.log('Hello'>'Hell')
+
+
+// var a = 0.3
+// var b = 0.1+0.2
+// console.log(b)
+// console.log(a === b)
+
+
+// Округлення в JavaScript
+// Math.floor - округляє вниз
+// Math.ceil - округляє вверх
+// Math.round - округляє як в математиці
+
+
+// var c = 1.5
+
+// console.log(Math.floor(c))
+// console.log(Math.ceil(c))
+// console.log(Math.round(c))
+// console.log(c)
+
+
+
+// var a = 0.3
+// var b = 0.1+0.2
+// console.log("To fixe: ", b.toFixed(1))
+// console.log(a === b)
+
+
+
+// console.log(typeof b.toFixed())
+// console.log(typeof  + b.toFixed())
+// console.log(a === +b)
